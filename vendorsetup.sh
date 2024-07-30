707d7615caf33ef924b5b693165a244a2d1a9511
Khalil.B <80763333+katakishi0@users.noreply.github.com>
2024-07-30 12:38:37 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_m170-user
-add_lunch_combo omni_m170-userdebug
-add_lunch_combo omni_m170-eng
+add_lunch_combo twrp_m170-user
+add_lunch_combo twrp_m170-userdebug
+add_lunch_combo twrp_ml170-eng
