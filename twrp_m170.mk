f600bf9d2cbb4c4822f4b73c77c30866299492c5
Khalil.B <80763333+katakishi0@users.noreply.github.com>
2024-07-30 15:31:42 -0300

    Update twrp_m170.mk

--- twrp_m170.mk
+++ twrp_m170.mk
@@ -9,6 +9,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from m170 device
+$(call inherit-product, device/motorola/m170/device.mk)
+
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
